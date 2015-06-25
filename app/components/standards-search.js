1223b6ad369e121dcaf2d44df92e0ebf686eccbb
Scott Messinger <scottmessinger@gmail.com>
2015-06-25 09:50:27 -0400

    Moving around files

---
+++ app/components/standards-search.js
@@ -0,0 +1,59 @@
+import Ember from 'ember';
+import _ from "npm:lodash";
+import hbs from 'htmlbars-inline-precompile';
+
+export default Ember.Component.extend({
+
+
+  isSearchVisible: Ember.computed('standardSetIds', function(){
+    return _.chain(this.get('standardSetIds'))
+            .filter(s => !_.contains(s, 'blank'))
+            .value().length > 0
+  }),
+
+
+  actions: {
+    addPane(){
+      this.sendAction('addPane')
+    },
+    selectSet(id, oldId){
+      this.sendAction('selectSet', id, oldId)
+    },
+    removeSet(id){
+      if (this.get('standardSetIds').length == 1) return;
+      this.sendAction('removeSet', id)
+    }
+  },
+
+  layout: hbs`
+  {{partial "navbar"}}
+
+  <div class="search-interface">
+
+    <div class="search-bar {{if isSearchVisible 'show'}}">
+      <div class="search-bar__icons">
+        {{!-- <div class="add-search-pane" {{action 'addPane'}}>
+          {{partial "icons/ios7-plus-outline"}} Add another set of standards to search within
+        </div> --}}
+      </div>
+      {{input value=query class="search-bar__input" placeholder="What would you like to search for?"}}
+      <div class="show-link" {{action 'showLink'}}>
+        {{partial "icons/ios7-link"}}
+      </div>
+    </div>
+
+    <div class="search-panes">
+      {{#each standardSetIds as |id|}}
+        {{standard-set id=id jurisdictions=jurisdictions selectSet="selectSet" class="standard-set" removeSet="removeSet"}}
+      {{/each}}
+      {{#if isSearchVisible}}
+        <div class="add-search-pane-button" {{action 'addPane'}}>
+          {{partial "icons/ios7-browser-outline"}}
+        </div>
+      {{/if}}
+    </div>
+  </div>
+  `
+
+
+})
