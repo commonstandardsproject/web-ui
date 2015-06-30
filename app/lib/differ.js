71ca0e62f12d308dd8a50acb2a79c795e058a958
Scott Messinger <scottmessinger@gmail.com>
2015-06-30 15:47:22 -0400

    add differ

---
+++ app/lib/differ.js
@@ -0,0 +1,90 @@
+import _ from "npm:lodash";
+// From:    https://github.com/Starcounter-Jack/JSON-Patch/blob/master/src/json-patch-duplex.js
+// Fetched: June 30, 2015
+
+export default function(mirror, obj){
+  var patches = {"$set": {}, "$unset": {}, "$push": {}}
+  generate(mirror, obj, patches, '')
+  return patches
+}
+
+function generate(mirror, obj, patches, path) {
+  var newKeys = _objectKeys(obj);
+  var oldKeys = _objectKeys(mirror);
+  var changed = false;
+  var deleted = false;
+
+  for (var t = oldKeys.length - 1; t >= 0; t--) {
+    var key = oldKeys[t];
+    var oldVal = mirror[key];
+    if (obj.hasOwnProperty(key)) {
+      var newVal = obj[key];
+      if (typeof oldVal == "object" && oldVal != null && typeof newVal == "object" && newVal != null) {
+        generate(oldVal, newVal, patches, path + "." + escapePathComponent(key));
+      } else {
+        if (oldVal != newVal) {
+          changed = true;
+          patches["$set"][(path + "." + escapePathComponent(key)).replace(/^\./, '')] = deepClone(newVal);
+        }
+      }
+    } else {
+      if (!_.isArray(obj)){
+        //   Because MongoDB can't do multiple push/pull, we can't do this.
+        //   patches["$pullAll"][(path).replace(/^\./, '')] = patches["$pullAll"][(path).replace(/^\./, '')] || []
+        //   patches["$pullAll"][(path).replace(/^\./, '')].push(deepClone(oldVal))
+        // } else {
+        patches["$unset"][(path + "." + escapePathComponent(key)).replace(/^\./, '')] = true;
+        deleted = true; // property has been deleted
+      }
+    }
+  }
+
+  if (!deleted && newKeys.length == oldKeys.length) {
+    return;
+  }
+
+  // if hash
+  if (_.isArray(obj)){
+    if (_objectKeys(mirror) !== oldKeys) {
+      patches["$set"][path.replace(/^\./, '')] = deepClone(obj)
+    }
+  } else {
+    for (var u = 0; u < newKeys.length; u++) {
+      var newKey = newKeys[u];
+      if (!mirror.hasOwnProperty(newKey)) {
+        patches["$set"][(path + "." + escapePathComponent(newKey)).replace(/^\./, '')] =  deepClone(obj[newKey])
+      }
+    }
+  }
+}
+
+function escapePathComponent(str) {
+  if (str.indexOf('/') === -1 && str.indexOf('~') === -1)
+  return str;
+  return str.replace(/~/g, '~0').replace(/\//g, '~1');
+}
+
+
+function deepClone(obj) {
+  if (typeof obj === "object") {
+    return JSON.parse(JSON.stringify(obj));
+  } else {
+    return obj;
+  }
+}
+
+
+var _objectKeys = (function () {
+  if (Object.keys)
+  return Object.keys;
+
+  return function (o) {
+    var keys = [];
+    for (var i in o) {
+      if (o.hasOwnProperty(i)) {
+        keys.push(i);
+      }
+    }
+    return keys;
+  };
+})();
---
+++ tests/unit/lib/differ-test.js
@@ -0,0 +1,40 @@
+import { moduleFor, test } from 'ember-qunit';
+import { module } from 'qunit';
+import differ from "../../../lib/differ";
+
+// moduleFor('lib:differ', 'Unit | Lib | differ', {
+//   // Specify the other units that are required for this test.
+//   // needs: ['service:foo']
+// });
+
+module('Unit | Lib | differ', {})
+
+// Replace this with your real tests.
+test('it exists', function(assert) {
+  assert.ok(differ)
+});
+
+test('add to hash', function(assert){
+  var diff = differ({}, {a: 1})
+  assert.deepEqual(diff, {"$set": {"a":   1}, "$push": {}, "$unset": {}})
+})
+
+test('finds differences in array', function(assert){
+  var diff = differ({a: [1, 2]}, {a: [1]})
+  assert.deepEqual(diff, {
+    "$push": {},
+    "$unset": {},
+    "$set": {
+      "a": [1]
+    }
+
+  })
+})
+
+test('$unset', function(assert){
+  var diff = differ({a: [1, 2]}, {})
+  assert.deepEqual(diff, {
+    "$set": {} ,
+    "$push": {},
+    "$unset": { a: true} })
+})
