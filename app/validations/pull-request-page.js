5fc47f22da091687e610aacdcc428c1e0fd912ee
Marika Ross <marika.ross@gmail.com>
2019-02-28 15:10:38 -0700

    [front] Add custom validation  messages.

--- app/validations/pull-request-page.js
+++ app/validations/pull-request-page.js
@@ -1,12 +1,15 @@
 import { validatePresence, validateFormat } from "ember-changeset-validations/validators"
 
 export default {
-  submitterName: validatePresence(true),
-  submitterEmail: validateFormat({ type: "email" }),
-  "standardSet.educationLevels": validatePresence(true),
-  "standardSet.document.sourceURL": validateFormat({ type: "url" }),
-  "standardSet.document.title": validateFormat(true),
-  "standardSet.standards.subject": validatePresence(true),
-  "standardSet.title": validatePresence(true),
-  "standardSet.jurisdiction.title": validatePresence(true),
+  submitterName: validatePresence({ presence: true, message: "please enter your name" }),
+  submitterEmail: validateFormat({ type: "email", message: "must be a valid email address" }),
+  "standardSet.educationLevels": validatePresence({ presence: true, message: "please select an education level" }),
+  "standardSet.document.sourceURL": validateFormat({ type: "url", message: "must be a valid URL" }),
+  "standardSet.document.title": validatePresence({ presence: true, message: "please enter the title of the source" }),
+  "standardSet.standards.subject": validatePresence({ presence: true, message: "please enter a subject" }),
+  "standardSet.title": validatePresence({ presence: true, message: "please enter the grade or course name" }),
+  "standardSet.jurisdiction.title": validatePresence({
+    presence: true,
+    message: "please select the name of the organization",
+  }),
 }
