1f4a39654e312bf30493f82bf0d51d6d7ccf9d27
Marika Ross <marika.ross@gmail.com>
2019-02-28 14:16:50 -0700

    [front] Update validations so they point to correct value

--- app/validations/pull-request-page.js
+++ app/validations/pull-request-page.js
@@ -1,12 +1,12 @@
 import { validatePresence, validateFormat } from "ember-changeset-validations/validators"
 
 export default {
-  "standardSet.submitterName": validatePresence(true),
-  "standardSet.submitterEmail": validateFormat({ type: "email" }),
+  submitterName: validatePresence(true),
+  submitterEmail: validateFormat({ type: "email" }),
   "standardSet.educationLevels": validatePresence(true),
   "standardSet.document.sourceURL": validateFormat({ type: "url" }),
   "standardSet.document.title": validateFormat(true),
   "standardSet.standards.subject": validatePresence(true),
-  "standardSet.standards.title": validatePresence(true),
+  "standardSet.title": validatePresence(true),
   "standardSet.jurisdiction.title": validatePresence(true),
 }
