a291f0c5682a4c0bb39ca647f4fc4ebc1f449c73
Marika Ross <marika.ross@gmail.com>
2019-03-13 18:57:36 -0600

    [front] Prevent truthy ambiguity.

--- app/validators/notation.js
+++ app/validators/notation.js
@@ -3,7 +3,7 @@ import _ from "npm:lodash"
 export default function validateNotation() {
   return (key, oldValue) => {
     let checkStandardNotation = _.filter(Object.values(oldValue), standard => {
-      return standard.statementNotation
+      return !_.isEmpty(standard.statementNotation)
     })
 
     let isValid = checkStandardNotation.length / Object.values(oldValue).length
