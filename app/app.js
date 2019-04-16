0db3053ae0a75834e6443bd01c6152d930d2f5b4
Marika Ross <marika.ross@gmail.com>
2019-04-16 10:56:31 -0600

    [front] Add error tracking with Sentry.

--- app/app.js
+++ app/app.js
@@ -2,6 +2,9 @@ import Ember from "ember"
 import Application from "@ember/application"
 import Resolver from "./resolver"
 import loadInitializers from "ember-load-initializers"
+import * as Sentry from "@sentry/browser"
+import { init } from "@sentry/browser"
+import * as Integrations from "@sentry/integrations"
 import config from "./config/environment"
 
 const App = Application.extend({
@@ -11,6 +14,14 @@ const App = Application.extend({
     paste: "paste",
   },
 })
+
+if (config.environment === "production") {
+  init({
+    dsn: "https://fc38237be543483c8faf57ebd6310e94@sentry.io/1440387",
+    environment: config.environment,
+    integrations: [new Integrations.Ember()],
+  })
+}
 
 loadInitializers(App, config.modulePrefix)
 
