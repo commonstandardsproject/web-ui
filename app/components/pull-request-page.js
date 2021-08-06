52017ca04e52970342674f9c936b983c12a2b280
Scott Ames-Messinger <scott@commoncurriculum.com>
2021-08-06 14:36:29 -0400

    logging

--- app/components/pull-request-page.js
+++ app/components/pull-request-page.js
@@ -358,6 +358,7 @@ export default Ember.Component.extend({
     },
 
     setCspStatus(value) {
+      console.log("set csp status", value, get(this, "model.standardSet.cspStatus.value"))
       set(this, "model.standardSet.cspStatus.value", value)
     },
 
