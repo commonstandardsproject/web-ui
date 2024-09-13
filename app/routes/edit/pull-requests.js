fd1c8d1d9f5362e89a495d9ada75c5654467f83a
Matthew Weidner <17693586+mweidner037@users.noreply.github.com>
2024-09-13 17:39:58 -0500

    fix remaining showSignIn

--- app/routes/edit/pull-requests.js
+++ app/routes/edit/pull-requests.js
@@ -1,25 +1,24 @@
 import Ember from "ember"
+import { storageFor } from "ember-local-storage"
 import Fetcher from "../../lib/fetcher"
 import resetScroll from "../../mixins/reset-scroll"
-import { storageFor } from "ember-local-storage"
 
 export default Ember.Route.extend(resetScroll, {
   authenticate: Ember.inject.service(),
   session: storageFor("persistedSession"),
 
-  isAuthenticated: Ember.computed("session.authenticatedAt", function() {
+  isAuthenticated: Ember.computed("session.authenticatedAt", function () {
     return Date.now() - this.get("session.authenticatedAt") < 3100000
   }),
 
   beforeModel(transition) {
     if (Ember.get(this, "isAuthenticated") === false) {
-      this.transitionTo("edit").then(
-        function() {
-          Ember.get(this, "authenticate").showSignin(function() {
-            transition.retry()
-          })
-        }.bind(this)
-      )
+      this.transitionTo("edit").then(() => {
+        Ember.get(this, "authenticate").show()
+        Ember.get(this, "authenticate.lock").on("authenticate", () => {
+          transition.retry()
+        })
+      })
     }
   },
 
