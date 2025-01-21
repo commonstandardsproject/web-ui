a7f2c3275befdad608a3f8c6484c8715fdfac039
Matthew Weidner <17693586+mweidner037@users.noreply.github.com>
2025-01-21 14:49:49 -0600

    remove async (causes regeneratorRuntime error in prod build) This is a known Babel issue. Instead of fighting with the build settings, I'm just removing our two low-priority uses of async.

--- app/components/standard-set.js
+++ app/components/standard-set.js
@@ -7,38 +7,7 @@ import hbs from "htmlbars-inline-precompile"
 import { storageFor } from "ember-local-storage"
 import Papa from "npm:papaparse"
 
-const saveFile = async (blob, suggestedName) => {
-  // Feature detection. The API needs to be supported
-  // and the app not run in an iframe.
-  const supportsFileSystemAccess =
-    'showSaveFilePicker' in window &&
-    (() => {
-      try {
-        return window.self === window.top;
-      } catch (err) {
-        return false;
-      }
-    })();
-  // If the File System Access API is supported…
-  if (supportsFileSystemAccess) {
-    try {
-      // Show the file save dialog.
-      const handle = await showSaveFilePicker({
-        suggestedName,
-      });
-      // Write the blob to the file.
-      const writable = await handle.createWritable();
-      await writable.write(blob);
-      await writable.close();
-      return;
-    } catch (err) {
-      // Fail silently if the user has simply canceled the dialog.
-      if (err.name !== 'AbortError') {
-        console.error(err.name, err.message);
-        return;
-      }
-    }
-  }
+const saveFile = (blob, suggestedName) => {
   // Fallback if the File System Access API is not supported…
   // Create the blob URL.
   const blobURL = URL.createObjectURL(blob);
